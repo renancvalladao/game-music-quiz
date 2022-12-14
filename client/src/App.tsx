5a5f9e1e78cf14450b547b508ff2e09419404773
Renan Valladão <renancvalladao@gmail.com>
2022-12-14 19:03:00 -0300

    Adding new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { InGame } from './pages/InGame'
+import { Lobby } from './pages/Lobby'
 import { Rooms } from './pages/Rooms'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="rooms" element={<Rooms />} />
+        <Route path="room" element={<Lobby />} />
+        <Route path="game" element={<InGame />} />
       </Routes>
     </ChakraProvider>
   )
