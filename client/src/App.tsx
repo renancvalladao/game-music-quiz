473bbf81f402e429050ea0b2ed7e96c619d8b302
Renan Valladão <renancvalladao@gmail.com>
2022-12-14 14:49:33 -0300

    Configuring chakra-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Rooms } from './pages/Rooms'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="rooms" element={<Rooms />} />
-    </Routes>
+    <ChakraProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="rooms" element={<Rooms />} />
+      </Routes>
+    </ChakraProvider>
   )
 }
 
