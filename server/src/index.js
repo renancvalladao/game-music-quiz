c61cccd6f805a56184ed670b52dad263bd718e46
Renan Valladão <renancvalladao@gmail.com>
2022-12-16 18:24:23 -0300

    Creating and listing rooms on server

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,33 @@
 const express = require('express')
-const app = express()
 const http = require('http')
-const server = http.createServer(app)
 const { Server } = require('socket.io')
+const { randomUUID } = require('crypto')
+
+const PORT = 3001
+
+const app = express()
+const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000'
   }
 })
 
-const PORT = 3001
+const rooms = []
 
 io.on('connection', (socket) => {
-  console.log('A user connected')
+  console.log(`User ${socket.id} connected`)
+
+  socket.on('room:list', (callback) => {
+    callback({ data: rooms })
+  })
+
+  socket.on('room:create', ({ name, host, config }) => {
+    const room = { id: randomUUID(), name, host, players: 1, config }
+    rooms.push(room)
+
+    io.emit('room:created', room)
+  })
 })
 
 server.listen(PORT, () => {
