e05145c7372071cccd3cc9b45cafd4d40bccea83
Renan Valladão <renancvalladao@gmail.com>
2022-12-19 19:19:45 -0300

    Adding more games

--- server/src/games.js
+++ server/src/games.js
@@ -1,3 +1,45 @@
+const banjooKazooie = {
+  details: {
+    title: 'Banjoo-Kazooie',
+    composer: 'Grant Kirkhope'
+  },
+  songs: [
+    {
+      name: 'Main Title',
+      url: 'https://www.youtube.com/watch?v=buOhUGJ9Pvc'
+    },
+    {
+      name: 'Overture',
+      url: 'https://www.youtube.com/watch?v=Lm4wplpESl4'
+    },
+    {
+      name: 'Spiral Mountain',
+      url: 'https://www.youtube.com/watch?v=IlzflkN7g-U'
+    }
+  ]
+}
+
+const castlevaniaSymphonyOfTheNight = {
+  details: {
+    title: 'Castlevania: Symphony of the Night',
+    composer: 'Michiru Yamane'
+  },
+  songs: [
+    {
+      name: 'Metamorphosis 1',
+      url: 'https://www.youtube.com/watch?v=HnVDnAMs_Mk'
+    },
+    {
+      name: 'Prologue',
+      url: 'https://www.youtube.com/watch?v=GQKRpJ2z9b8'
+    },
+    {
+      name: 'Dance of Illusions',
+      url: 'https://www.youtube.com/watch?v=fe-XLye-r5A'
+    }
+  ]
+}
+
 const chronoTrigger = {
   details: {
     title: 'Chrono Trigger',
@@ -6,26 +48,41 @@ const chronoTrigger = {
   songs: [
     {
       name: 'Presentiment',
-      url: 'https://www.youtube.com/watch?v=JU1rM8UYpus'
+      url: 'https://www.youtube.com/watch?v=Ja0saZrHkXk'
+    },
+    {
+      name: 'Chrono Trigger',
+      url: 'https://www.youtube.com/watch?v=wR91d9itVZ4'
+    },
+    {
+      name: 'Morning Sunlight',
+      url: 'https://www.youtube.com/watch?v=Tmc0fFzIzKk'
     },
     {
       name: 'Wind Scene',
-      url: 'https://www.youtube.com/watch?v=AbIwWBi1-UA'
+      url: 'https://www.youtube.com/watch?v=iN4TSrFHyRg'
     },
     {
-      name: 'Secret of the Forest',
-      url: 'https://www.youtube.com/watch?v=j6VoS1RlA6Q'
+      name: "Frog's Theme",
+      url: 'https://www.youtube.com/watch?v=q7229hdxcSM'
     },
     {
       name: 'Corridors of Time',
-      url: 'https://www.youtube.com/watch?v=pIjNrhKyg2k'
+      url: 'https://www.youtube.com/watch?v=HVBWTC085uA'
     }
   ]
 }
 
 const darkSouls = {
-  details: { title: 'Dark Souls', composer: 'Motoi Sakuraba' },
+  details: {
+    title: 'Dark Souls',
+    composer: 'Motoi Sakuraba'
+  },
   songs: [
+    {
+      name: 'Bell Gargoyle',
+      url: 'https://www.youtube.com/watch?v=ac8QTcY0G0E'
+    },
     {
       name: 'Great Grey Wolf Silf',
       url: 'https://www.youtube.com/watch?v=zKu9rferBFU'
@@ -41,6 +98,105 @@ const darkSouls = {
   ]
 }
 
-const games = [chronoTrigger, darkSouls]
+const persona4 = {
+  details: {
+    title: 'Persona 4',
+    composer: 'Shoji Meguro'
+  },
+  songs: [
+    {
+      name: 'Pursuing My True Self',
+      url: 'https://www.youtube.com/watch?v=cBWmvxc4L5I'
+    },
+    {
+      name: 'A Corner of Memories',
+      url: 'https://www.youtube.com/watch?v=iV1o6tbZLZk'
+    },
+    {
+      name: 'Your Affection',
+      url: 'https://www.youtube.com/watch?v=d-x3ojOGWa8'
+    },
+    {
+      name: "I'll Face Myself",
+      url: 'https://www.youtube.com/watch?v=rV4BlAlrIlk'
+    },
+    {
+      name: 'Heartbeat, Heartbreak',
+      url: 'https://www.youtube.com/watch?v=ZGyVct4wi2E'
+    }
+  ]
+}
+
+const superMario64 = {
+  details: {
+    title: 'Super Mario 64',
+    composer: 'Koji Kondo'
+  },
+  songs: [
+    {
+      name: 'Title Theme',
+      url: 'https://www.youtube.com/watch?v=pYAjvWCVkH0'
+    },
+    {
+      name: 'Inside the Castle Walls',
+      url: 'https://www.youtube.com/watch?v=ZkFpUQc3Y2o'
+    },
+    { name: 'File Select', url: 'https://www.youtube.com/watch?v=hrJNgLGbYg0' }
+  ]
+}
+
+const theLegendOfZeldaOcarinaOfTime = {
+  details: {
+    title: 'The Legend of Zelda: Ocarina of Time',
+    composer: 'Koji Kondo'
+  },
+  songs: [
+    {
+      name: 'Title Theme',
+      url: 'https://www.youtube.com/watch?v=N22STVAxL5M'
+    },
+    {
+      name: 'Enter Ganondorf',
+      url: 'https://www.youtube.com/watch?v=dyF6wkV0P10'
+    },
+    {
+      name: 'Windmill Hut',
+      url: 'https://www.youtube.com/watch?v=u6KQzVZ092Y'
+    }
+  ]
+}
+
+const undertale = {
+  details: { title: 'Undertale', composer: 'Toby Fox' },
+  songs: [
+    {
+      name: 'Once Upon a Time',
+      url: 'https://www.youtube.com/watch?v=s7RRgF5Ve_E'
+    },
+    {
+      name: 'Hopes and Dreams',
+      url: 'https://www.youtube.com/watch?v=tz82xbLvK_k'
+    },
+    {
+      name: 'Snowy',
+      url: 'https://www.youtube.com/watch?v=8BIeGdBjJiU'
+    },
+    {
+      name: 'Megalovania',
+      url: 'https://www.youtube.com/watch?v=c5daGZ96QGU'
+    }
+  ]
+}
+
+const games = [
+  banjooKazooie,
+  castlevaniaSymphonyOfTheNight,
+  chronoTrigger,
+  darkSouls,
+  persona4,
+  superMario64,
+  theLegendOfZeldaOcarinaOfTime,
+  undertale
+]
 
 module.exports = games
