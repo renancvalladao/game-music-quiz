3f2b77141156152a4ee35d0ee7b50a1ea983da92
Renan Valladão <renancvalladao@gmail.com>
2022-12-19 21:26:04 -0300

    Adding rooms link

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,12 @@
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')}>
-      <Flex h={16}></Flex>
+      <Flex h={16}>
+        <Link to={'rooms'}>Rooms</Link>
+      </Flex>
     </Box>
   )
 }
