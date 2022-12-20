ae8f04c001bddcd0188de8aec1803959de13f74f
Renan Valladão <renancvalladao@gmail.com>
2022-12-19 21:57:22 -0300

    Fixing cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://game-music-quiz.netlify.app/']
+    origin: ['http://localhost:3000', 'https://game-music-quiz.netlify.app']
   }
 })
 
