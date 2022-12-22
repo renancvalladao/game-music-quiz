f86f20e529f80b250ffde82066a2b27955d015bb
Renan Valladão <renancvalladao@gmail.com>
2022-12-22 17:26:47 -0300

    Improving NavBar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,15 @@
-import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Box, Flex, HStack, Text, useColorModeValue } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')}>
-      <Flex h={16}>
-        <Link to={'/rooms'}>Rooms</Link>
+      <Flex h={16} px={64} alignItems={'center'}>
+        <Text fontSize={'4xl'}>GMQ</Text>
+        <HStack spacing={'10'} ml={20}>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/rooms'}>Rooms</Link>
+        </HStack>
       </Flex>
     </Box>
   )
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,6 @@ export const Home = () => {
   return (
     <>
       <NavBar />
-      <div>Home</div>
     </>
   )
 }
