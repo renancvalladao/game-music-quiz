0c7585029413584be99cbdd88c99379d722d588a
Renan Valladão <renancvalladao@gmail.com>
2023-01-09 21:48:00 -0300

    Listing soundtracks

---
+++ client/src/components/SountrackCard.tsx
@@ -0,0 +1,120 @@
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  ExternalLinkIcon,
+  InfoOutlineIcon
+} from '@chakra-ui/icons'
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Collapse,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  Text,
+  Tooltip,
+  useColorModeValue,
+  useDisclosure
+} from '@chakra-ui/react'
+
+type Song = {
+  name: string
+  video_url: string
+}
+
+type SoundtrackCardProps = {
+  gameId: number
+  gameName: string
+  loaded: boolean
+  songs: Song[]
+  setSongs: (songs: Song[]) => void
+}
+
+export const SoundtrackCard = ({
+  gameId,
+  gameName,
+  loaded,
+  songs,
+  setSongs
+}: SoundtrackCardProps) => {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <Card
+      w="100%"
+      bgColor={useColorModeValue('white', 'gray.700')}
+      h={'fit-content;'}
+    >
+      <CardHeader>
+        <Heading size="md">{gameName}</Heading>
+      </CardHeader>
+      <Collapse in={isOpen} animateOpacity>
+        <CardBody bg={useColorModeValue('gray.300', 'gray.600')}>
+          <Box
+            overflowY={'auto'}
+            maxH={'116px'}
+            sx={{
+              '&::-webkit-scrollbar': {
+                width: '8px'
+              },
+              '&::-webkit-scrollbar-thumb': {
+                background: useColorModeValue('blue.400', 'blue.600'),
+                borderRadius: '4px'
+              }
+            }}
+          >
+            {songs.map((song) => (
+              <Flex key={song.name} alignItems={'center'} py={1}>
+                <Text>{song.name}</Text>
+                <Link href={song.video_url} ml={2} isExternal>
+                  <Flex alignItems={'center'}>
+                    <ExternalLinkIcon />
+                  </Flex>
+                </Link>
+              </Flex>
+            ))}
+          </Box>
+        </CardBody>
+      </Collapse>
+      <CardFooter justifyContent={'space-between'} alignItems={'center'}>
+        {loaded ? (
+          <Tooltip hasArrow label={'Total songs'} placement="top">
+            <Flex alignItems={'center'}>
+              <InfoOutlineIcon />
+              <Text ml={1}>{songs.length}</Text>
+            </Flex>
+          </Tooltip>
+        ) : (
+          <Tooltip hasArrow label={'Not loaded'} placement="top">
+            <Flex alignItems={'center'}>
+              <InfoOutlineIcon />
+            </Flex>
+          </Tooltip>
+        )}
+        <IconButton
+          aria-label="Fetch soundtrack"
+          icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          variant={'unstyled'}
+          onClick={() => {
+            if (loaded) {
+              onToggle()
+              return
+            }
+            fetch(
+              `${process.env.SERVER || 'http://localhost:3001'}/songs/${gameId}`
+            )
+              .then((resp) => resp.json())
+              .then((data) => {
+                setSongs(data)
+                onToggle()
+              })
+          }}
+        />
+      </CardFooter>
+    </Card>
+  )
+}
--- client/src/pages/Soundtracks.tsx
+++ client/src/pages/Soundtracks.tsx
@@ -1,9 +1,104 @@
+import { SearchIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Center,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  SimpleGrid,
+  Spinner,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { NavBar } from '../components/NavBar'
+import { SoundtrackCard } from '../components/SountrackCard'
+
+type GameInfo = {
+  id: number
+  name: string
+  loaded: boolean
+  songs?: {
+    name: string
+    video_url: string
+  }[]
+}
 
 export const Soundtracks = () => {
+  const [games, setGames] = useState<GameInfo[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(`${process.env.SERVER || 'http://localhost:3001'}/games`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setGames(
+          data.map((game: { id: number; name: string }) => {
+            return { ...game, loaded: false }
+          })
+        )
+        setIsLoading(false)
+      })
+  }, [])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setSearchValue(event.target.value)
+
   return (
     <>
       <NavBar />
+      <Box px={64} pt={8} pb={8}>
+        <InputGroup size={'md'} mb={8}>
+          <InputLeftElement pointerEvents="none" children={<SearchIcon />} />
+          <Input
+            _hover={{ bg: '' }}
+            _focus={{ bg: useColorModeValue('white', 'gray.700') }}
+            bgColor={useColorModeValue('white', 'gray.700')}
+            variant={'filled'}
+            value={searchValue}
+            onChange={handleChange}
+            placeholder="Search games..."
+          />
+        </InputGroup>
+        {isLoading ? (
+          <Center w={'100%'} pt={8}>
+            <Spinner size="xl" color="blue.500" thickness="4px" />
+          </Center>
+        ) : (
+          <SimpleGrid
+            spacing={6}
+            templateColumns="repeat(auto-fill, minmax(384px, 1fr))"
+          >
+            {games
+              .filter((game) =>
+                game.name
+                  .toLocaleLowerCase()
+                  .includes(searchValue.toLocaleLowerCase())
+              )
+              .map((game) => (
+                <SoundtrackCard
+                  key={game.name}
+                  gameId={game.id}
+                  gameName={game.name}
+                  songs={game.songs || []}
+                  loaded={game.loaded}
+                  setSongs={(songs) => {
+                    setGames((prevGames) => {
+                      const selectedGame = prevGames.find(
+                        (prevGame) => prevGame.id === game.id
+                      )
+                      if (!selectedGame) return prevGames
+                      selectedGame.songs = songs
+                      selectedGame.loaded = true
+                      return [...prevGames]
+                    })
+                  }}
+                />
+              ))}
+          </SimpleGrid>
+        )}
+      </Box>
     </>
   )
 }
